f5f078bcf17d1868028d1a11da7a1cc45a3678e7
AydanGaite <aksg823@gmail.com>
2023-08-20 01:29:10 -0700

    chore: add global tsconfig

--- packages/config/eslint-config-custom/index.js
+++ packages/config/eslint-config-custom/index.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["turbo", "prettier"],
+  plugins: ['prettier'],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
 };
