787751985370ec18ee1815f263614c89befa19ce
AydanGaite <aksg823@gmail.com>
2023-09-13 01:48:50 -0700

    fix: default init of input value

--- packages/shared/src/components/IO/utils/Input.ts
+++ packages/shared/src/components/IO/utils/Input.ts
@@ -3,9 +3,13 @@ import { InputBuilder, InputSocketParams } from '.';
 
 export default class Input<T = unknown> {
   constructor(params: InputSocketParams, tree: StatefulTree) {
-    const value: T | undefined = undefined;
-
     const { type, connection: defaultConnection } = params;
+
+    const value: T | undefined = defaultConnection
+      ? (tree.nodes?.[defaultConnection.nodeId]?.outputs?.[
+          defaultConnection.outputId
+        ].value as T)
+      : undefined;
 
     const input = new InputBuilder({
       type,
