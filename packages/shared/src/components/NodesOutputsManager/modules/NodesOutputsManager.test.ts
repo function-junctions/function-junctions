315a13624408a0953354e5e5d5ca1579eeb22ae8
voltean <victor_oltean@sfu.ca>
2023-10-15 21:03:11 -0700

    Started to implement deleteOutput (with tests).

--- packages/shared/src/components/NodesOutputsManager/modules/NodesOutputsManager.test.ts
+++ packages/shared/src/components/NodesOutputsManager/modules/NodesOutputsManager.test.ts
@@ -1,9 +1,13 @@
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 import {
   NodesOutputsTreeBuilder,
   SerializedNodesOutputsTree,
 } from '@/components/NodesOutputsTreeBuilder';
 import { NodesOutputsManager } from '.';
+import {
+  NodesInputsTreeBuilder,
+  SerializedNodesInputsTree,
+} from '@/components/NodesInputsTreeBuilder';
 
 const outputsTree: SerializedNodesOutputsTree = {
   nodes: {
@@ -14,14 +18,43 @@ const outputsTree: SerializedNodesOutputsTree = {
         },
       },
     },
+    c: {
+      outputs: {
+        outputIdC: {
+          value: `Hello again!`,
+        },
+      },
+    },
+  },
+};
+
+const inputsTree: SerializedNodesInputsTree = {
+  nodes: {
+    a: {
+      inputs: {
+        inputIdA: {
+          connection: {
+            nodeId: `c`,
+            outputId: `outputIdC`,
+          },
+        },
+      },
+    },
   },
 };
 
 describe('Nodes output manager', () => {
-  test('check to see if new node can be created', () => {
-    const outputs = new NodesOutputsTreeBuilder(outputsTree);
-    const manager = new NodesOutputsManager(outputs);
+  let outputs: NodesOutputsTreeBuilder;
+  let inputs: NodesInputsTreeBuilder;
+  let manager: NodesOutputsManager;
 
+  beforeEach(() => {
+    outputs = new NodesOutputsTreeBuilder(outputsTree);
+    inputs = new NodesInputsTreeBuilder(inputsTree, outputs);
+    manager = new NodesOutputsManager(outputs, inputs);
+  });
+
+  test('check to see if new node can be created', () => {
     manager.createOutput('b', {
       id: 'string',
       value: 'Hello!',
@@ -29,15 +62,47 @@ describe('Nodes output manager', () => {
 
     expect(outputs.value.nodes.b.outputs.string.value).toBe('Hello!');
   });
-  test('check to see if new node respects existing nodes', () => {
-    const outputs = new NodesOutputsTreeBuilder(outputsTree);
-    const manager = new NodesOutputsManager(outputs);
 
+  test('check to see if new node respects existing nodes', () => {
     expect(() =>
       manager.createOutput('a', {
         id: 'string',
         value: 'Overwritten!',
       }),
     ).toThrowError();
+  });
+
+  test.skip('check that deleting an output that does not exist does nothing', () => {
+    manager.deleteOutput('a', 'notExistingOutput');
+
+    expect(outputs.value.nodes.a.outputs.string.value).toBe('Hello!');
+    expect(inputs.value.nodes.a.inputs.inputIdA.value.connection).toEqual({
+      nodeId: `c`,
+      outputId: `outputIdC`,
+    });
+    expect(outputs.value.nodes.c.outputs.outputIdC.value).toBe('Hello again!');
+  });
+
+  test.skip('check if an output is deleted from a node with no references to the output', () => {
+    manager.deleteOutput('a', 'string');
+
+    // a's output should be deleted
+    expect(outputs.value.nodes.a.outputs.string).toBeUndefined();
+    // other nodes should not be affected
+    expect(outputs.value.nodes.c.outputs.outputIdC.value).toBe('Hello again!');
+    expect(inputs.value.nodes.a.inputs.inputIdA.value.connection).toEqual({
+      nodeId: `c`,
+      outputId: `outputIdC`,
+    });
+  });
+
+  test.skip('check if an output and input references to the output are removed', () => {
+    // c's output is referenced by a's input
+    manager.deleteOutput('c', 'outputIdC');
+
+    expect(outputs.value.nodes.c.outputs.outputIdC).toBeUndefined();
+    expect(
+      inputs.value.nodes.a.inputs.inputIdA.value.connection,
+    ).toBeUndefined();
   });
 });
--- packages/shared/src/components/NodesOutputsManager/modules/NodesOutputsManager.ts
+++ packages/shared/src/components/NodesOutputsManager/modules/NodesOutputsManager.ts
@@ -1,12 +1,22 @@
 import merge from 'lodash/merge';
+import { entries, isEqual, trim, values } from 'lodash';
 import { NodesOutputsTreeBuilder } from '@/components/NodesOutputsTreeBuilder';
 import { SerializedOutput } from '@/components/Output';
+import {
+  NodeInputsTree,
+  NodesInputsTreeBuilder,
+} from '@/components/NodesInputsTreeBuilder';
 
 export default class NodesOutputsManager {
-  public outputTree: NodesOutputsTreeBuilder;
+  public readonly outputTree: NodesOutputsTreeBuilder;
+  public readonly inputTree: NodesInputsTreeBuilder;
 
-  constructor(outputTree: NodesOutputsTreeBuilder) {
+  constructor(
+    outputTree: NodesOutputsTreeBuilder,
+    inputTree: NodesInputsTreeBuilder,
+  ) {
     this.outputTree = outputTree;
+    this.inputTree = inputTree;
   }
 
   public createOutput = <T = unknown>(
@@ -35,4 +45,33 @@ export default class NodesOutputsManager {
       },
     });
   };
+
+  public deleteOutput = (nodeId: string, outputId: string): void => {
+    const { nodes: inputNodes } = this.inputTree.value;
+    const { nodes: outputNodes } = this.outputTree.value;
+    if (!(nodeId in outputNodes)) return;
+
+    const targetOutputNode = outputNodes[nodeId];
+    if (!(outputId in targetOutputNode.outputs)) return;
+
+    entries(inputNodes)
+      .filter(([, nodeInputs]) =>
+        // update to also get the input id if referenced
+        this.doesNodeReferenceOutput(nodeInputs, outputId),
+      )
+      .forEach(() => {
+        // delete the input
+      });
+
+    // updated to set the value using reduce
+    delete targetOutputNode.outputs[outputId];
+  };
+
+  private doesNodeReferenceOutput = (
+    nodeInputs: NodeInputsTree,
+    outputId: string,
+  ): boolean =>
+    values(nodeInputs.inputs).some((input) =>
+      isEqual(trim(input.value.connection?.outputId), trim(outputId)),
+    );
 }
